7a04e89ca579ad9180b5874a0b39b8a54519d991
João Vasco <dev.jndvasco@gmail.com>
2021-09-23 17:12:09 +0100

    Working capturing images

--- D435i/main.cpp
+++ D435i/main.cpp
@@ -4,65 +4,91 @@
 #include <librealsense2/rs.hpp> // Include RealSense Cross Platform API
 #include <fstream>              // File IO
 #include <iostream>             // Terminal IO
-#include <sstream>              // Stringstreams
+#include <string>
 #include <chrono>               // Time
 
-using namespace std;
+#define _SILENCE_EXPERIMENTAL_FILESYSTEM_DEPRECATION_WARNING
 
-// 3rd party header for writing png files
+#include <experimental/filesystem>
+
 #define STB_IMAGE_WRITE_IMPLEMENTATION
 
 #include "stb_image_write.h"
 
+namespace fs = std::experimental::filesystem;
+using namespace std;
+
 long millis();
 
-// This sample captures 30 frames and writes the last frame to disk.
-// It can be useful for debugging an embedded system with no display.
+string outFolder = "Output\\";
+// string outRGB = "RGB\\";
+// string outDepth = "Depth\\";
+string filepath = "C:\\Documentos 2\\CLion\\Cameras\\";
+
+const int photoCount = 20; //Number of frames to capture
+
 int main(int argc, char *argv[]) try
 {
-    cout << "Passei aqui" << endl;
+  long captureTimeLog[photoCount];
 
-    cout << millis() << endl;
+  outFolder = filepath + outFolder;
 
-    rs2::colorizer color_map;  // Declare depth colorizer for pretty visualization of depth data
-    rs2::pipeline pipe; // Declare RealSense pipeline, encapsulating the actual device and sensors
-    pipe.start();     // Start streaming with default recommended configuration
+  if (!fs::is_directory(outFolder) || !fs::exists(outFolder))
+  { // Check if src folder exists
+    fs::create_directory(outFolder); // create src folder
+    cout << "Pasta Criada em: " << outFolder << endl;
+  }
 
-    // Capture 30 frames to give autoexposure, etc. a chance to settle
-    for (auto i = 0; i < 30; ++i) pipe.wait_for_frames();
+  rs2::colorizer color_map;  // Declare depth colorizer for pretty visualization of depth data
+  rs2::pipeline pipe; // Declare RealSense pipeline, encapsulating the actual device and sensors
+  pipe.start();     // Start streaming with default recommended configuration
 
-    // Wait for the next set of frames from the camera. Now that autoexposure, etc.
-    // has settled, we will write these to disk
+  for (auto i = 0; i < 30; ++i) pipe.wait_for_frames(); //Let the cam output stabilize
+
+  long startMillis = millis();
+  long oldMillis = startMillis;
+
+  for (int i = 0; i < 20; ++i)
+  {
+    oldMillis = millis();
     for (auto &&frame: pipe.wait_for_frames())
     {
-        // We can only save video frames as pngs, so we skip the rest
-        if (auto vf = frame.as<rs2::video_frame>())
-        {
-            // Use the colorizer to get an rgb image for the depth stream
-            if (vf.is<rs2::depth_frame>()) vf = color_map.process(frame);
+      // We can only save video frames as pngs, so we skip the rest
+      if (auto vf = frame.as<rs2::video_frame>())
+      {
+        if (vf.is<rs2::depth_frame>()) continue;
+        //if (vf.is<rs2::depth_frame>()) vf = color_map.process(frame);
 
-            // Write images to disk
-            stringstream png_file;
-            png_file << "rs-save-to-disk-output-" << vf.get_profile().stream_name() << ".png";
-            stbi_write_png(png_file.str().c_str(), vf.get_width(), vf.get_height(),
-                           vf.get_bytes_per_pixel(), vf.get_data(), vf.get_stride_in_bytes());
-            cout << "Saved " << png_file.str() << endl;
-        }
+        stringstream png_file;
+        png_file << outFolder << "RS_D435i_" << (millis() - startMillis) << vf.get_profile().stream_name() << ".png";
+        stbi_write_png(png_file.str().c_str(), vf.get_width(), vf.get_height(),
+                       vf.get_bytes_per_pixel(), vf.get_data(), vf.get_stride_in_bytes());
+        cout << "Saved " << png_file.str() << endl;
+      }
+      captureTimeLog[i] = millis() - oldMillis;
     }
-    cout<< millis() << endl;
+  }
+  cout << millis() << endl;
 
-    return EXIT_SUCCESS;
+  cout << "==== Duracoes ====" << endl;
+
+  for (int i = 0; i < photoCount; ++i)
+  {
+    cout << "Duracao " << i << " :" << captureTimeLog[i] << endl;
+  }
+
+  return EXIT_SUCCESS;
 }
 catch (const rs2::error &e)
 {
-    cerr << "RealSense error calling " << e.get_failed_function() << "(" << e.get_failed_args() << "):\n    "
-              << e.what() << endl;
-    return EXIT_FAILURE;
+  cerr << "RealSense error calling " << e.get_failed_function() << "(" << e.get_failed_args() << "):\n    "
+       << e.what() << endl;
+  return EXIT_FAILURE;
 }
 catch (const exception &e)
 {
-    cerr << e.what() << endl;
-    return EXIT_FAILURE;
+  cerr << e.what() << endl;
+  return EXIT_FAILURE;
 }
 
 
@@ -74,5 +100,5 @@ catch (const exception &e)
 //======================================================================
 long millis()
 {
-    return chrono::duration_cast<chrono::milliseconds>(chrono::system_clock::now().time_since_epoch()).count();
+  return chrono::duration_cast<chrono::milliseconds>(chrono::system_clock::now().time_since_epoch()).count();
 }//End millis
