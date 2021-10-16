850a4a4c5fdc502403ed40716dab9e0d8c00c2d3
João Vasco <jndvasco@gmail.com>
2021-10-17 00:07:14 +0100

    Multithreaded image capture

--- Both/main.cpp
+++ Both/main.cpp
@@ -2,11 +2,20 @@
 // Created by João Vasco on 15/10/2021.
 //================================================================
 #include <iostream>
-#include <thread>
 #include <chrono>
+#include <thread>
+#include <functional>
+#include <atomic>
+
+#include "include/utils.hpp"
+
 
 //Zed Libs
 #include <sl/Camera.hpp>
+
+std::atomic<int> zedFrameCount;
+
+sl::String outputPath("C:/Documentos 2/CLion/Cameras/Both/output/test.svo");
 
 
 /* Timing const so we now when to capture each frame
@@ -17,15 +26,18 @@ const int fpsOutput = 30;
 const int millisBetweenFrames = 1000 / fpsOutput;
 
 /* Aux functions declaration */
+void
+threadTimerZed(std::function<void(sl::Camera &zedObject)> inputFunction, unsigned int interval, sl::Camera &zedObject);
 bool initZed(sl::Camera &zedObject);
 void initIntel();
-void threadOne();
+void zedFrameCapture(sl::Camera &zedObject);
 void threadTwo();
 
 
 int main()
 {
-  std::cout << "Final FPS: " << fpsOutput << std::endl;
+  std::cout << "[INFO] - Final FPS: " << fpsOutput << std::endl;
+  std::cout << "[INFO] - Time between frames: " << millisBetweenFrames << std::endl;
 
   std::cout << "Starting Zed camera" << std::endl;
   sl::Camera zedCam;
@@ -35,21 +47,32 @@ int main()
     return -1;
   }
 
-  std::cout << "Starting Intel camera" << std::endl;
+  zedFrameCount = 0;
+
+  /*std::cout << "Starting Intel camera" << std::endl;
   sl::Camera intelCam;
 
   if (!initZed(zedCam))
   {
     return -1;
-  }
+  }*/
 
+  //Put the zed capturing in the background
+  threadTimerZed(zedFrameCapture, millisBetweenFrames, zedCam);
 
-  std::thread th1(threadOne);
-  std::thread th2(threadTwo);
+  SetCtrlHandler(); //Capture CTRL + C so we know when to exit
+  while (!exit_app); //Wait unitl we want to leave the app
 
+  //Wait for 2 seconds and let other threads to finishi before closing
+  std::this_thread::sleep_for(std::chrono::milliseconds(2000));
 
-  th1.join();
-  th2.join();
+  zedCam.disableRecording(); //Stop the video stream
+  zedCam.close(); //Close the data stream and leave the cam available
+
+  //Print some information
+  std::cout << "===============================" << std::endl;
+  std::cout << "Frames Capturados: " << zedFrameCount << std::endl;
+  std::this_thread::sleep_for(std::chrono::milliseconds(2000));
   return 0;
 } //End main()
 
@@ -83,7 +106,7 @@ bool initZed(sl::Camera &zedObject)
 
   auto returned_state = zedObject.open(init_parameters); //Open the camera data stream
 
-  if (returned_state != sl::ERROR_CODE::SUCCESS)
+  if (returned_state != sl::ERROR_CODE::SUCCESS) // If it failed print the error message and exit
   {
     std::cout << "[FATAL ERROR] - Error starting Zed. " << sl::toVerbose(returned_state) << std::endl;
     return false;
@@ -91,17 +114,61 @@ bool initZed(sl::Camera &zedObject)
   else
   {
     std::cout << "[INFO] - Zed camera started with sucess" << std::endl;
-    return true;
   }
+
+  // Start the video recording
+  // If you set the compression mode to H264 or H265 you will need a compatible NVIDIA GPU
+
+  sl::RecordingParameters rec_parameters;
+  rec_parameters.video_filename = outputPath;
+  rec_parameters.compression_mode = sl::SVO_COMPRESSION_MODE::LOSSLESS;
+
+  returned_state = zedObject.enableRecording(rec_parameters);
+
+  if (returned_state != sl::ERROR_CODE::SUCCESS)// If it failed print the error message and exit
+  {
+    std::cout << "[FATAL ERROR] - Error starting recording. " << sl::toVerbose(returned_state) << std::endl;
+    zedObject.close();
+    return false;
+  }
+
+  return true;
 }
 
 
-void threadOne()
+void
+threadTimerZed(std::function<void(sl::Camera &zedObject)> inputFunction, unsigned int interval, sl::Camera &zedObject)
 {
-  std::cout << "Printed from thread one!" << std::endl;
+  std::thread([inputFunction, interval, &zedObject]()
+              {
+                  while (true)
+                  {
+                    auto x = std::chrono::steady_clock::now() + std::chrono::milliseconds(interval);
+                    inputFunction(zedObject);
+                    std::this_thread::sleep_until(x);
+                  }
+              }).detach();
+}
+
+
+void zedFrameCapture(sl::Camera &zedObject)
+{
+  //sl::RecordingStatus rec_status;
+  if (zedObject.grab() == sl::ERROR_CODE::SUCCESS)
+  {
+    zedFrameCount++;
+    std::cout << "Frame count: " << zedFrameCount << std::endl;
+  }
 }
 
 void threadTwo()
 {
+  long time = std::chrono::time_point_cast<std::chrono::milliseconds>(
+    std::chrono::steady_clock::now()).time_since_epoch().count();
   std::cout << "Printed from thread two!" << std::endl;
+  std::cout << time << std::endl;
+  std::cout << "====================" << std::endl;
 }
+
+
+//======================================================================================================================
